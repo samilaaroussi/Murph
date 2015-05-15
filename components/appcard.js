77a89d7cf69fb6cfec6c7c378939e3d087131e6d
Sami <sami@slt.fr>
2015-05-15 09:54:09 +0200

    clean code

--- components/appcard.js
+++ components/appcard.js
@@ -1,5 +1,5 @@
 var WDJAppCard = React.createClass({
- 
+
     render: function(){
         return (
             <div className="card">
@@ -7,10 +7,10 @@ var WDJAppCard = React.createClass({
                     <div className="icon">
                         <img src='icon' width="50px" height="auto" alt='Title' />
                     </div>
-                    <div className="title">{this.props.name}</div></a>
+                    <div className="title">{this.props.name}</div>
+                </a>
                     <div className="description">{this.props.desc}</div>
                     <a href="javascript:void(0);" className="button install"><i></i><span>安装</span></a>
-                </a>
             </div>
         );
     }
