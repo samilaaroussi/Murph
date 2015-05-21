d7720b288217bcaf7e606b196def331329d56273
Sami <sami@slt.fr>
2015-05-21 15:55:31 +0200

    appcard list component beginning

---
+++ components/appcardlist.js
@@ -0,0 +1,13 @@
+ var WDJAppCardList = React.createClass({
+
+    render: function(){
+        return (
+            <ul>
+                {this.props.children}
+            </ul>
+
+
+        );
+
+    }
+});
