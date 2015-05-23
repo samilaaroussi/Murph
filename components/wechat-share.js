b24bdbea5025b009b93ae14e59479591b20670cf
Sami <sami@slt.fr>
2015-05-23 13:28:06 +0200

    add spaces

--- components/wechat-share.js
+++ components/wechat-share.js
@@ -5,6 +5,7 @@ var WDJShareWechat = React.createClass({
         if (device.isP4 && campaignTools.isInstalled('com.tencent.mm')){
             res = campaignTools.runAppShare(this.props.title, this.props.desc, this.props.url, 'WECHAT');
         }
+        
         else {
             res = <a href={'http://www.wandoujia.com/qr?c=' + this.props.url} className={this.props.icon} alt={this.props.title}><i></i></a>;
         }
