c56ff83b5d3ad8f4d4de189b59d971ce66f2a1f4
Sami <sami@slt.fr>
2015-07-03 06:50:56 +0800

    Revert "Stilr Implementation"
    This reverts commit f33f1d3d6db065112b45c6bb9997f75c1cd6dd9e.

--- components/qrcode.jsx
+++
@@ -1,9 +0,0 @@
-var WDJQRCode = React.createClass({
-
-    render: function(){
-        
-        return (
-            <img src={'http://www.wandoujia.com/qr?c=' + this.props.content + '&s=' + this.props.size}/>
-        );
-    }
-});
