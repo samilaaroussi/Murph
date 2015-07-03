0c6c69c643fdaaac76a9728ef64d2efad163367a
Sami <sami@slt.fr>
2015-07-03 12:20:23 +0800

    Stilr implementation

---
+++ components/modal.jsx
@@ -0,0 +1,27 @@
+var WDJModal = React.createClass({
+
+    componentDidMount: function() {
+
+      $(function () {
+          $('#element').on('click', function () {
+              $.fn.custombox( this );
+              return false;
+          });
+      });
+
+    },
+
+    render: function () {
+
+        return (
+                <div id="openModal" className="modalDialog">
+                    <div className="container">
+                        <a href="#close" title="Close" className="close">X</a>
+                        {this.props.children}
+                    </div>
+                </div>
+
+        );
+    }
+
+});
