9b8877fbde05ffa3fc3106fb61cf8f9d6f9b231e
Sami <sami@slt.fr>
2015-07-22 12:44:55 +0800

    code style

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,55 +1,55 @@
 module.exports = function (grunt) {
-	grunt.initConfig({
-		browserify: {
+    grunt.initConfig({
+        browserify: {
 
-		  	components: {
-		    	src: [],
-		        dest: 'components-bundled.js',
-    			options: {
-    	        	debug: true,
-    	        	transform: ['reactify'],
-    	        	browserifyOptions: {
-		        		require: ['./components.js']
-		        	},
-		        	require: ['react']
-    	        }
-		    },
+            components: {
+                src: [],
+                dest: 'components-bundled.js',
+                options: {
+                    debug: true,
+                    transform: ['reactify'],
+                    browserifyOptions: {
+                        require: ['./components.js']
+                    },
+                    require: ['react']
+                }
+            },
 
-		    app: {
-		        src: 'app2.js',
-		        dest: 'app2-bundled.js',
-				options: {
-		        	debug: true,
-		        	transform: ['reactify'],
-		        	browserifyOptions: {
-		        		external: ['./components.js']
-		        	},
-		        	external: ['react']
-		        }
-		    }
-		},
+            app: {
+                src: 'app2.js',
+                dest: 'app2-bundled.js',
+                options: {
+                    debug: true,
+                    transform: ['reactify'],
+                    browserifyOptions: {
+                        external: ['./components.js']
+                    },
+                    external: ['react']
+                }
+            }
+        },
 
-    	watch: {
-    		
-      		components: {
-        		files: ['components/*.jsx'],
-        		tasks: ['browserify:components']
-      		},
+        watch: {
+            
+            components: {
+                files: ['components/*.jsx'],
+                tasks: ['browserify:components']
+            },
 
-      		app: {
-        		files: ['app*.js'], 
-        		tasks: ['browserify:app']
-      		},
+            app: {
+                files: ['app*.js'], 
+                tasks: ['browserify:app']
+            },
 
-  			options: {
-  		    	livereload: true
-  		    }
-    	}
-	});
+            options: {
+                livereload: true
+            }
+        }
+    });
 
-	grunt.loadNpmTasks('grunt-browserify');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.registerTask('build', ['browserify:components','browserify:app']);
-	grunt.registerTask('server', ['browserify:components','browserify:app']);
+    grunt.registerTask('build', ['browserify:components','browserify:app']);
+    grunt.registerTask('server', ['browserify:components','browserify:app']);
 };
